06ae20a624c9e6891d7f653eea21b8134c7b5c1c
ubata-mamezou <masato-ubata@mamezou.com>
2024-12-19 17:40:15 +0900

    静的解析の組み込み

--- order/src/main/java/com/mau/consider/cicd/order/Application.java
+++ order/src/main/java/com/mau/consider/cicd/order/Application.java
@@ -3,11 +3,19 @@ package com.mau.consider.cicd.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * BootStrap.
+ */
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  /**
+   * main.
+   *
+   * @param args 起動引数
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
